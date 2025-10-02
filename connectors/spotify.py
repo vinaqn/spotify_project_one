43db1353c12574090891f064ef86bf3c6100b458
vinaqn <vina3nguyen@gmail.com>
2025-10-02 04:40:49 -0700

    first git commit

---
+++ connectors/spotify.py
@@ -0,0 +1,38 @@
+import requests
+
+
+class SpotifyApiClient:
+    def __init__(self, client_id:str, client_secret:str):
+        self.base_url="https://api.spotify.com/v1"
+
+        if client_id is None:
+            raise Exception("API key cannot be set to None.")
+        self.client_id = client_id
+
+        if client_secret is None:
+            raise Exception("API secret key cannot be set to None.")
+        self.client_secret = client_secret
+
+        #Request Access Token
+        #The Client Credentials flow is used in server-to-server authentication. 
+        # Since this flow does not include authorization, only endpoints that do not access user information can be accessed.
+        auth_url = "https://accounts.spotify.com/api/token"
+        headers = {
+            "Content-Type": "application/x-www-form-urlencoded"
+        }
+        body = {"grant_type": "client_credentials",
+                "client_id":client_id,
+                "client_secret":client_secret}
+        
+        #Request Access Token
+        auth_response = requests.post(auth_url, headers=headers, data=body)
+        auth_response.raise_for_status()
+        self.access_token=auth_response.json()['access_token']
+        self.token_type=auth_response.json()['token_type']
+
+
+    def get_artist(self,artist_id:str):
+        header={"Authorization": f"{self.token_type} {self.access_token}"}
+        response=requests.get(f"{self.base_url}/artists/{artist_id}",headers=header)
+        return print(response.json())
+    
---
+++ pipeline/pipeline.py
@@ -0,0 +1,14 @@
+from dotenv import load_dotenv
+from connectors.spotify import SpotifyApiClient
+import os
+
+load_dotenv()
+
+API_KEY_ID = os.environ.get("spotify_client_id")
+API_SECRET_KEY = os.environ.get("spotify_client_secret")
+
+
+spotify_client=SpotifyApiClient(API_KEY_ID,API_SECRET_KEY)
+
+spotify_client.get_artist("7GlBOeep6PqTfFi59PTUUN")
+
