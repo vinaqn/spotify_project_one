48e047433b124bab95d7e48f2859a4946bd44bc1
vinaqn <vina3nguyen@gmail.com>
2025-10-03 19:22:48 -0700

    class PostgreSqlClient to connect to database

---
+++ connectors/postgres.py
@@ -0,0 +1,34 @@
+from sqlalchemy import create_engine
+from sqlalchemy.engine import URL
+from sqlalchemy.dialects import postresql
+
+class PosgreSqlClient:
+    """a client to connect to a postgresql database"""
+    
+    def __init__(self,
+                 server_name: str,
+                 database_name:str, 
+                 username: str,
+                 password: str,
+                 port: int =5432
+                 ):
+        
+        #set the database details
+        self.host_name=server_name
+        self.database_name=database_name
+        self.username=username
+        self.password=password
+        self.port=port
+
+        #connection url to pass into create engine
+        connection_url = URL.create(
+            drivername="postgresql+pg8000",
+            username=username,
+            password=password,
+            host=server_name,
+            port=port,
+            database=database_name,
+        )
+
+        #engine
+        self.engine=create_engine(connection_url)
