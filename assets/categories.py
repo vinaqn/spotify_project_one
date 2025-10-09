57eecfcd73cb77f46375487c2ecb4f877ad21123
Tommissed <telaurel@gmail.com>
2025-10-09 07:12:13 -0400

    added albums to pipeline

--- assets/categories.py
+++ assets/categories.py
@@ -56,31 +56,3 @@ def load_categories(PostgresSqlClient: PostgreSqlClient, list:list):
     PostgresSqlClient.engine.execute(upsert_statement)
 
     print('uploaded to database')
-
-
-#tests 
-# load_dotenv()
-
-# API_KEY_ID = os.environ.get("spotify_client_id")
-# API_SECRET_KEY = os.environ.get("spotify_client_secret")
-
-# #database details
-# DB_SERVER_NAME= os.environ.get("DB_SERVER_NAME")
-# DB_DATABASE_NAME = os.environ.get("DB_DATABASE_NAME")
-# DB_USERNAME = os.environ.get("DB_USERNAME")
-# DB_PASSWORD = os.environ.get("DB_PASSWORD")
-# DB_PORT = os.environ.get("DB_PORT")
-
-# spotify_client=SpotifyApiClient(API_KEY_ID,API_SECRET_KEY)
-
-# postgres_client=PostgreSqlClient(db_server_name=DB_SERVER_NAME,
-#                                 db_database_name=DB_DATABASE_NAME,
-#                                 db_username=DB_USERNAME,
-#                                 db_password=DB_PASSWORD,
-#                                 db_port=DB_PORT)
-
-
-
-# categories = get_categories(SpotifyApiClient=spotify_client)
-
-# load_categories(PostgresSqlClient=postgres_client,list=categories)
